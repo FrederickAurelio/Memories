77150963bcf88b94b05efd47911927a5825c7720
Frederick Aurelio Halim <frederick.ah88@gmail.com>
2025-04-10 10:44:10 +0800

    Frontend: designing layout for design page

--- frontend/app/app/design/[canvaId]/page.tsx
+++ frontend/app/app/design/[canvaId]/page.tsx
@@ -1,6 +1,15 @@
-async function page({ params }: { params: Promise<{ canvaId: string }> }) {
+async function Design({ params }: { params: Promise<{ canvaId: string }> }) {
   const { canvaId } = await params;
-  return <div>Canva Desig: {canvaId}</div>;
+  return (
+    <div className="grid h-full w-full grid-cols-[repeat(14,minmax(0,1fr))]">
+      <div className="col-span-1 bg-red-100">TOOLBOX</div>
+      <div className="col-start-2 col-end-[15] flex w-full items-center justify-center p-1">
+        <div className="aspect-video h-full bg-white shadow-[0_1px_20px_rgba(38,38,38,0.20)]">
+          CANVA
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default page;
+export default Design;
--- frontend/app/app/layout.tsx
+++ frontend/app/app/layout.tsx
@@ -5,8 +5,8 @@ async function LayoutApp({ children }: { children: React.ReactNode }) {
   // const pathname = headerList.get("x-current-path");
   return (
     <div className="flex h-dvh w-full bg-neutral-200">
-      <Sidebar/>
-      <div className="mr-3 mt-3 w-full rounded-t-2xl bg-neutral-100 px-4 py-2 shadow-lg drop-shadow-lg">
+      <Sidebar />
+      <div className="mr-3 mt-3 w-full rounded-t-2xl bg-neutral-50 px-4 py-2 shadow-lg drop-shadow-lg">
         {children}
       </div>
     </div>
